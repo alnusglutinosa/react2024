bf851fc58240fc2434c8b80ff7f520f7c526ff7e
Olha <olga.app2@gmail.com>
2024-08-15 21:30:10 +0300

    lesson_1

---
+++ hw_1/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+
+const Heading = ({ text='', color }) => {
+  return <h1 style={ {color}}>{text}</h1>
+}
+
+const Description = ({ textStyle }) => {
+  const userName = prompt('Please enter your name');
+
+  return <h2 style={ {fontStyle: textStyle}}>
+      {userName ? `${userName}'s` : 'My'} first React application.
+    </h2>
+}
+
+createRoot(document.getElementById('root')).render(<>
+  <Heading text="Hello, world!" color="crimson"/>
+  <Description textStyle="italic" />
+</>);
